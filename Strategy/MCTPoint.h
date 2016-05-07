d00fc381e4f36e104f474198019a41a5d1835f69
ykdsir <ykd14@outlook.com>
2016-05-07 17:10:49 +0800

    宝宝心里苦啊……重新写了函数

--- Strategy/MCTPoint.h
+++ Strategy/MCTPoint.h
@@ -5,11 +5,9 @@ extern int m;
 extern int n;
 class MCTNode{
 public:
-	int **board;
-	int *top;
 	int left,right,father;
 	int x,y;
-	bool isLeaf,isUser;
+	bool isUser;
 	double interest;//收益，暂定为赢的次数
 	int totalRound;
 
@@ -17,13 +15,6 @@ public:
 		interest = 0;
 		totalRound = 0;
 		x = y = -1;		
-		isLeaf = true;
-	}
-	void  ini(int **board,int* t,int lastX,int lastY){
-		top = t;
-		this->board = board;
-		x = lastX;
-		y = lastY;
 	}
 };
 
